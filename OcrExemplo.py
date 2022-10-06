e07fad30dcbc355bb9a024daaea5267ee28bc20c
Roger Schmidt <roger.schp@hotmail.com>
2022-10-06 11:05:38 -0300

    updating comments

--- OcrExemplo.py
+++ OcrExemplo.py
@@ -1,14 +1,17 @@
 import cv2
 import pytesseract
 
-# Lendop image com openCV
+# Lendo image com openCV
+# Read image with openCV
 img = cv2.imread("Capture.PNG")
 
 # Definindo o local que o tesseract esta instalado (OBS: O TESSERACT TEM QUE ESTAR INSTALADO PREVIAMENTE NO SISTEMA.)
-# LINK DO TESSERACT : https://github.com/UB-Mannheim/tesseract/wiki  DATA 06/10/22
+# Setting the path where tesseract is installed (Observation: Tesseract must be previously installed in the system.)
+# LINK TESSERACT : https://github.com/UB-Mannheim/tesseract/wiki  Date 06/10/22
 pytesseract.pytesseract.tesseract_cmd = "W:\Program Files\Tesseract-OCR\Tesseract.exe"
 
 # Utilizando o tesseract para transformar a image em string
+# Using tesseract to transform a image into string
 result = pytesseract.image_to_string(img)
 
 print(result)
